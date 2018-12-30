eece626c8ded9f4d97278ca7c134dc89e8d16f71
evgeniy <evgeniy.logvinov@cyoda.com>
2018-12-30 10:46:37 +0300

    Add GVUser and GVQuestion to project. Get this information from database system

---
+++ app/src/main/java/com/oleg/givevoice/user/GVUserAdapter.java
@@ -0,0 +1,73 @@
+package com.oleg.givevoice.user;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+
+import com.oleg.givevoice.MainActivity;
+import com.oleg.givevoice.R;
+
+/**
+ * Adapter to bind a GVUser List to a view
+ */
+public class GVUserAdapter extends ArrayAdapter<GVUser> {
+
+    /**
+     * Adapter context
+     */
+    Context mContext;
+
+    /**
+     * Adapter View layout
+     */
+    int mLayoutResourceId;
+
+    public GVUserAdapter(Context context, int layoutResourceId) {
+        super(context, layoutResourceId);
+
+        mContext = context;
+        mLayoutResourceId = layoutResourceId;
+    }
+
+    /**
+     * Returns the view for a specific item on the list
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        final GVUser currentItem = getItem(position);
+
+        if (row == null) {
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            row = inflater.inflate(mLayoutResourceId, parent, false);
+        }
+
+        row.setTag(currentItem);
+        final CheckBox checkBox = (CheckBox) row.findViewById(R.id.checkGVUser);
+        checkBox.setText(currentItem.getText());
+        checkBox.setChecked(false);
+        checkBox.setEnabled(true);
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (checkBox.isChecked()) {
+                    checkBox.setEnabled(false);
+                    if (mContext instanceof MainActivity) {
+                        MainActivity activity = (MainActivity) mContext;
+                        activity.checkItem(currentItem);
+                    }
+                }
+            }
+        });
+
+        return row;
+    }
+
+}
